c2c42b3f0ece7b8b6b04eb9b9fd4a85e28cabf0e
Vishnu Varthan <vichu@Vishnus-MacBook-Air.local>
2020-03-29 11:24:13 +0530

    Made React Coomand Work

---
+++ README.md
@@ -0,0 +1,1 @@
+# boilerplate-generator README
---
+++ src/extension.ts
@@ -0,0 +1,103 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+const spawnCommand = require('spawn-command');
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function run(cmd: string, options: any) {
+	return new Promise((accept, reject) => {
+		var opts: any = {};
+		let process = spawnCommand(cmd, options);
+
+		process.stdout.on('data', () => {
+			vscode.window.withProgress({
+				location: vscode.ProgressLocation.Notification,
+				title: "I am long running!",
+				cancellable: true
+			}, (progress, token) => {
+				token.onCancellationRequested(() => {
+					console.log("User canceled the long running operation");
+				});
+
+				progress.report({ increment: 0, message: "Your Project is being Created" });
+
+
+
+				var p = new Promise(resolve => {
+					setTimeout(() => {
+						resolve();
+					}, 5000);
+				});
+
+				return p;
+			});
+		});
+		process.stderr.on('data', (err: any) => {
+			vscode.window.showInformationMessage(err);
+		})
+		process.on('exit', () => {
+			vscode.window.showInformationMessage("Project Created Succesfully");
+		});
+	});
+}
+export function activate(context: vscode.ExtensionContext) {
+
+	const workspace = vscode.workspace;
+	if (!workspace) {
+		vscode.window.showErrorMessage("No workspace");
+		return;
+	}
+
+	const frameWorksSupported: string[] = ['React JS', 'Django', 'Vue', 'Flutter', "HTML", "Chrome Extension"];
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "boilerplate-generator" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		let cwd = vscode.workspace.rootPath;
+		const options: any = {};
+		options.cwd = cwd;
+		vscode.window.showInformationMessage('Hello Worlddd');
+
+		const quickPick = vscode.window.createQuickPick();
+		quickPick.items = frameWorksSupported.map((elem: string) => ({ label: elem }));
+		quickPick.onDidChangeSelection(async ([selection]) => {
+			if (selection) {
+				vscode.window.showInformationMessage(selection.label);
+				const result = await vscode.window.showInputBox({
+					value: 'abcdef',
+					valueSelection: [2, 4],
+					placeHolder: 'For example: fedcba. But not: 123',
+
+				});
+				if (selection.label === "React JS") {
+					run(`npx create-react-app ${result}`, options);
+					console.log('hi');
+
+
+				}
+				else if (selection.label === "Django") {
+					run(`django-admin startproject ${result}`, options);
+				}
+
+
+
+
+			}
+		});
+		quickPick.onDidHide(() => quickPick.dispose());
+		quickPick.show();
+
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
